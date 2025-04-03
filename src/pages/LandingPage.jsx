34eea3b6e9c01fc1310097bb4b43ef78d702237d
Muhammad Raihan Resa <muhraihanresa243@gmail.com>
2025-04-03 23:38:49 +0800

    landing page(layanan section)

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,44 +4,90 @@ import Photo1 from '../assets/photo/1.png'
 import Photo2 from '../assets/photo/2.png'
 import Photo3 from '../assets/photo/3.png'
 import Photo4 from '../assets/photo/4.png'
+import Layanan1 from '../assets/layanan/1.png'
+import Layanan2 from '../assets/layanan/2.png'
+import Layanan3 from '../assets/layanan/3.png'
+import Layanan4 from '../assets/layanan/4.png'
+import Background from '../assets/bgmount.png'
 
 const LandingPage = () => {
     return (
-        <div className='px-60 py-10'>
-            <div className='flex gap-10 justify-center items-center relative'>
-                <img src={Icon1} className='h-10' />
-                <div className='relative'>
-                    <h1 className='text-3xl text-[#FFC100]'>Tentang Kami</h1>
-                    <img
-                        src={Icon1}
-                        className='h-20 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-25'
-                    />
+        <div className='py-10'>
+            <div className='px-60'>
+                <div className='flex gap-10 justify-center items-center'>
+                    <img src={Icon1} className='h-10' />
+                    <div className='relative'>
+                        <h1 className='text-3xl text-[#FFC100]'>Tentang Kami</h1>
+                        <img
+                            src={Icon1}
+                            className='h-20 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-25'
+                        />
+                    </div>
+                    <img src={Icon1} className='h-10' />
                 </div>
 
+                <div className="flex justify-between items-center mt-10 gap-10">
+                    <div className="grid grid-cols-2 gap-4 w-1/3">
+                        <img src={Photo1} className="h-30 object-cover rounded-lg" />
+                        <img src={Photo2} className="h-40 object-cover rounded-lg" />
+                        <img src={Photo3} className="h-40 object-cover rounded-lg" />
+                        <img src={Photo4} className="h-30 object-cover rounded-lg" />
+                    </div>
 
-                <img src={Icon1} className='h-10' />
+                    <div className="w-2/3">
+                        <p className="text-justify">
+                            <span className='text-[#FFC100] font-semibold'>GaPakeRem Adventure</span>, singkatan dari Gerakan Pecinta Alam Kreatif Makassar, adalah komunitas dan operator wisata yang bergerak di bidang pemanduan
+                            pendakian gunung di Sulawesi Selatan. Didirikan oleh Yasin Habibi di Makassar pada 25 Agustus 2021, komunitas ini menyediakan layanan open trip dan
+                            private trip, khususnya untuk kawasan pegunungan di Sulawesi Selatan. Dengan mengedepankan kreativitas, setiap kegiatan selalu didokumentasikan dalam
+                            bentuk konten atau vlog yang menarik, memberikan kenangan yang abadi bagi para peserta. Didukung oleh tim profesional yang terdiri dari dua guide
+                            bersertifikasi APGI (Asosiasi Pemandu Gunung Indonesia) dan sepuluh porter berpengalaman, GakPakeRem Adventure terbuka untuk semua kalangan tanpa
+                            batasan, menjadikan pengalaman pendakian gunung lebih inklusif, aman, dan berkesan.
+                        </p>
+                    </div>
+                </div>
             </div>
 
+            <div className="relative min-h-screen mt-10 px-60 py-10">
+                <img src={Background} alt="background" className="absolute top-0 left-0 w-full h-full z-0" />
 
-            <div className="flex justify-between items-center mt-10 gap-10">
-                <div className="grid grid-cols-2 gap-4 w-1/3">
-                    <img src={Photo1} className="h-30 object-cover rounded-lg" />
-                    <img src={Photo2} className="h-40 object-cover rounded-lg" />
-                    <img src={Photo3} className="h-40 object-cover rounded-lg" />
-                    <img src={Photo4} className="h-30 object-cover rounded-lg" />
+                <div className='mt-40 flex items-center gap-10 relative z-0'>
+                    <h1 className='text-3xl font-bold text-gray-500'>Layanan trip Kami</h1>
+                    <div className='h-5 w-2/3 bg-[#FFC100] rounded-lg' />
                 </div>
 
-                <div className="w-2/3">
-                    <p className="text-justify">
-                        <span className='text-[#FFC100] font-semibold'>GaPakeRem Adventure</span>, singkatan dari Gerakan Pecinta Alam Kreatif Makassar, adalah komunitas dan operator wisata yang bergerak di bidang pemanduan
-                        pendakian gunung di Sulawesi Selatan. Didirikan oleh Yasin Habibi di Makassar pada 25 Agustus 2021, komunitas ini menyediakan layanan open trip dan
-                        private trip, khususnya untuk kawasan pegunungan di Sulawesi Selatan. Dengan mengedepankan kreativitas, setiap kegiatan selalu didokumentasikan dalam
-                        bentuk konten atau vlog yang menarik, memberikan kenangan yang abadi bagi para peserta. Didukung oleh tim profesional yang terdiri dari dua guide
-                        bersertifikasi APGI (Asosiasi Pemandu Gunung Indonesia) dan sepuluh porter berpengalaman, GakPakeRem Adventure terbuka untuk semua kalangan tanpa
-                        batasan, menjadikan pengalaman pendakian gunung lebih inklusif, aman, dan berkesan.
-                    </p>
+                <div className='mt-20 mb-20 grid grid-cols-2 gap-10 relative z-10'>
+                    <div className='flex flex-col gap-2 items-center'>
+                        <div className='w-20 h-20 bg-gray-500 rounded-full overflow-hidden'>
+                            <img src={Layanan1} alt='layanan1' className='w-full h-full object-cover' />
+                        </div>
+                        <h4 className='font-semibold text-gray-500'>Pendaki Open Trip</h4>
+                        <p className='text-sm'>Pendakian bersama grup terbuka untuk umum dengan pemanduan profesional, cocok bagi individu yang ingin menikmati pendakian seru dan bertemu teman baru.</p>
+                    </div>
+                    <div className='flex flex-col gap-2 items-center'>
+                        <div className='w-20 h-20 bg-gray-500 rounded-full overflow-hidden'>
+                            <img src={Layanan2} alt='layanan2' className='w-full h-full object-cover' />
+                        </div>
+                        <h4 className='font-semibold text-gray-500'>Pendaki Private Trip</h4>
+                        <p className='text-sm'>Layanan pendakian eksklusif untuk grup dengan fleksibilitas rute dan waktu yang dapat disesuaikan sesuai kebutuhan.</p>
+                    </div>
+                    <div className='flex flex-col gap-2 items-center'>
+                        <div className='w-20 h-20 bg-gray-500 rounded-full overflow-hidden'>
+                            <img src={Layanan3} alt='layanan3' className='w-full h-full object-cover' />
+                        </div>
+                        <h4 className='font-semibold text-gray-500'>Pemandu Bersertifikat</h4>
+                        <p className='text-sm'>Pendakian dipandu oleh guide bersertifikat APGI yang berpengalaman, memastikan pengalaman aman, nyaman dan menyenangkan.</p>
+                    </div>
+                    <div className='flex flex-col gap-2 items-center'>
+                        <div className='w-20 h-20 bg-gray-500 rounded-full overflow-hidden'>
+                            <img src={Layanan4} alt='layanan4' className='w-full h-full object-cover' />
+                        </div>
+                        <h4 className='font-semibold text-gray-500'>Jasa Porter</h4>
+                        <p className='text-sm'>Layanan porter untuk membawa alat dan logistik, memudahkan peserta fokus pada pengalaman pendakian.</p>
+                    </div>
                 </div>
+
             </div>
+
         </div>
     )
 }
